71d1091cffd32e20ffca299c3f82709155a6b541
Andrei Tomiuc <tomiuc@thinkdesquared.com>
2024-04-24 17:15:37 +0300

    feat: add new alias

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -331,6 +331,7 @@ module.exports = function (webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        'react-native-sound': 'react-native-web-sound',
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
